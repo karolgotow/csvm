a8f827f3c266a8f397e7bc3d99bd81fdb602e4b6
karolgotow <karol.gotowala@proton.me>
2026-01-25 01:41:47 +0000

    Update csvm.h

--- csvm.h
+++ csvm.h
@@ -1,7 +1,24 @@
-#define MIN_ROWS 2
+/*#define MIN_ROWS 2
 #define MAX_ROWS 10
 #define MIN_COLS 2
-#define MAX_COLS 10
+#define MAX_COLS 10*/
+
+typedef enum {
+    MIN_ROWS = 2,
+    MAX_ROWS = 10,
+    MIN_COLS = 2,
+    MAX_COLS = 10
+} rowsy_i_colsy;
+
+typedef enum {
+    MENU_GENERUJ = 1,
+    MENU_WYSWIETL,
+    MENU_MODYFIKUJ,
+    MENU_PODSUMUJ,
+    MENU_ZAPISZ,
+    MENU_WCZYTAJ, //do tego miejsca sie same inkrementuja o 1 
+    MENU_KONIEC = 0
+} menu_option;
 
 //CVector* alokuj_vector(int size);
 
@@ -39,3 +56,7 @@ void usun_kolumne(CMatrix *mat);
 void zwolnij_vector(CMatrix *mat);
 
 CVector *alokuj_vector(int size);
+
+void zapisz_matrix_do_pliku(CMatrix *mat);
+
+CMatrix *wczytaj_matrix_z_pliku();
